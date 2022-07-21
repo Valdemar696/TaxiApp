dc661fb769dc8b5cc59c6fc2795f3bcad0b15623
Valdemar696 <valdemar.tigrov@gmail.com>
2022-07-22 00:13:11 +0300

    Commit message

---
+++ app/src/main/java/com/example/taxiapp/DriverSignInActivity.java
@@ -0,0 +1,104 @@
+package com.example.taxiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class DriverSignInActivity extends AppCompatActivity {
+
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputName;
+    private TextInputLayout textInputPassword;
+    private TextInputLayout textInputConfirmPassword;
+
+    private Button loginSignUpButton;
+    private TextView toggleLoginSignUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_sign_in);
+
+        textInputEmail = findViewById(R.id.textInputEmail);
+        textInputName = findViewById(R.id.textInputName);
+        textInputPassword = findViewById(R.id.textInputPassword);
+        textInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+
+        loginSignUpButton = findViewById(R.id.loginSignUpButton);
+        toggleLoginSignUpTextView = findViewById(R.id.toggleLoginSignUpTextView);
+
+    }
+
+    private boolean validateName() { // Проверяем корректность ввода имени
+        String nameInput = textInputName.getEditText().getText().toString().trim();
+        //Получаем EditText/получаем непосредственно сам текст/переводим в String/обрезаем пробелы
+
+        if (nameInput.isEmpty()) {
+            textInputName.setError("Please, input your name");
+            return false;
+        } else if (nameInput.length() >15) {
+            textInputName.setError("Name length have to be less than 15");
+            return false;
+        }
+        else {
+            textInputName.setError("");
+            return true;
+        }
+    }
+
+    private boolean validateEmail() { // Проверяем корректность ввода мыла
+        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+        //Получаем EditText/получаем непосредственно сам текст/переводим в String/обрезаем пробелы
+
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Please, input your email");
+            return false;
+        } else {
+            textInputEmail.setError("");
+            return true;
+        }
+    }
+
+    private boolean validatePassword() { // Проверяем корректность пароля
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+        //Получаем EditText/получаем непосредственно сам текст/переводим в String/обрезаем пробелы
+        String confirmPasswordInput = textInputConfirmPassword.getEditText().getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Please, input your password");
+            return false;
+        } else if (passwordInput.length() < 7) {
+            textInputPassword.setError("Password length have to be more than 6");
+            return false;
+        } else if (!passwordInput.equals(confirmPasswordInput)) {
+            textInputPassword.setError("Passwords have to match");
+            return false;
+        }
+        else {
+            textInputName.setError("");
+            return true;
+        }
+    }
+
+    public void loginSignUpUser(View view) {
+        if (!validateEmail() | !validateName() | !validatePassword()) {
+            return;
+        }
+
+        String userInput =  "Name: " + textInputName.getEditText().getText().toString().trim() + "\n" +
+                            "Email: " + textInputEmail.getEditText().getText().toString().trim() + "\n" +
+                            "Password: " + textInputPassword.getEditText().getText().toString().trim() + "\n";
+
+
+        Toast.makeText(this, userInput, Toast.LENGTH_LONG).show();
+    }
+
+    public void toggleLoginSignUp(View view) {
+    }
+}
--- app/src/main/java/com/example/taxiapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/taxiapp/SplashScreenActivity.java
@@ -2,6 +2,7 @@ package com.example.taxiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -10,5 +11,26 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread() { //создаём поток для 3-х секундной заставки
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace(); // если не получилось поспать 3 сек - выводим ошибку
+                } finally { // после сна запускаем другое действие (try-catch-finally, ну сам знаешь)
+                    startActivity(new Intent(SplashScreenActivity.this, ChooseModeActivity.class));
+                }
+            }
+        };
+
+        thread.start(); // вызываем вышеуказанный поток
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
